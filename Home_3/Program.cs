4614ef1b323604730552ea0b25b2435a6a558acf
Alex <sanyasan123@mail.ru>
2022-08-09 18:39:12 +0300

    Home_3

---
+++ Home_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 2;
+
+if(b == 0)
+    {
+    Console.Write("число " + a + " четное");
+    }
+
+else
+{
+   Console.Write("число " + a + " нечетное");
+}
