26e4b3855d6ebd8ddad84a20358e7b5c61fe8d0e
Alex <sanyasan123@mail.ru>
2022-08-09 18:57:52 +0300

    Home_4

---
+++ Home_4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 2)
+{
+    Console.Write("Повторите попытку. Введите число больше 1");
+}
+
+while(n > 1)
+{
+    int b = n % 2; 
+    if(b == 0)
+    {
+    Console.Write(n + " ");
+    }
+    n = n - 1;  
+}
+
