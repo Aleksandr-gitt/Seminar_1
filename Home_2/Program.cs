e61df63aa581d9810abe9971c158169ff6153bfe
Alex <sanyasan123@mail.ru>
2022-08-09 18:26:48 +0300

    Home_2

---
+++ Home_2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a > b) 
+{
+    if(a > c) 
+    {
+        Console.Write("max = " + a);
+    }
+    else 
+    {
+        Console.Write("max = " + c);
+    }
+}
+
+if(a < b) 
+{
+    if(b > c) 
+    {
+        Console.Write("max = " + b);
+    }
+
+    else 
+    {
+        Console.Write("max = " + c);
+    }
+}
+
