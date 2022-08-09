9e60fcbcc26149af89f3194e57159b2f1b9817f2
Alex <sanyasan123@mail.ru>
2022-08-09 17:53:57 +0300

    Home_1

--- Home_1/Program.cs
+++ Home_1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b) {
+    Console.Write("max = " + a);
+}
+
+else
+{
+    Console.Write("max = " + b);
+}
+ 
